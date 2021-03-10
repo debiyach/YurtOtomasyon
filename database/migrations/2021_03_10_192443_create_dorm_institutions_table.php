b1dd72200f016259d4b417e7a13eb88f4592a09a
Debiyach <omerfarukgol@hotmail.com>
2021-03-10 22:32:55 +0300

    Yurt Kurumu tablosu oluşturuldu.

--- database/migrations/2021_03_10_192443_create_dorm_institutions_table.php
+++ database/migrations/2021_03_10_192443_create_dorm_institutions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDormInstitutionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('dorm_institutions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedInteger('dorm');
-            $table->rememberToken();
+            $table->string('email');
+            $table->string('tel_no');
+            $table->string('FAX');
+            $table->string('address');
+            $table->enum('is_active',[1,0]);
+            $table->json('photograph');
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('dorm_institutions');
     }
 }
