2b786c3996b5b77302468b3afa96cfaf04e41470
Tarık Ay <helix257@gmail.com>
2021-04-02 21:05:09 +0300

    yapılacaklar güncellendi

--- Yapilacaklar.php
+++ Yapilacaklar.php
@@ -4,5 +4,13 @@ $yapilacaklar = [
     'Yurt türü olacak kız ve erkek olarak. Uyuşan cinsiyetler yurda girebilecek.',
     'Cinsiyetlere göre tablolar düzenlenecek, Tablolar = [Kurum,Ogrenci,Personel]',
     'Yurt yataklar ve odalar tablosu oluşturulacak. Aynı kişi iki odada veya yatakta görünemeyecek.'
+    'müdür cikis yapma durumu düzeltilecek'
+
 ];
-$yapilanlar = [];
+$yapilanlar = ['login islemleri yapılandırıldı-idea'
+'model isimleri düzenlendi-config'
+'middlewarelar olusturuldu-database'
+'giris sayfalari düzenlendi-public'
+'middleware hatalar düzeltildi-app'
+'route olusturuldu-resources'
+'route olusturuldu-routes'];
