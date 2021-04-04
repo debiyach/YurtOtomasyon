8f0ebcb92b9c829767a93e49b10bce8cb3a09d89
Tarık Ay <helix257@gmail.com>
2021-04-05 00:21:41 +0300

    yapilacaklar güncellendi  anlatılacaklar eklendi.

---
+++ Anlatılacaklar.php
@@ -0,0 +1,11 @@
+<?php
+
+$anlatilanlar = [ ];
+
+$anlatilacaklar = [
+'dokümantasyonu hazırlamaya basladık, template olusturuldu, tablolarda gelistirmeler yaptik,',
+'Base apicontroller ve requestleri yapılandırdık.',
+'PersonelApi ilgili mail ve api geliştirmesi yapıldı.Öğrenci eklenirken öğrenciye şifreyle mail gönderildi.',
+'Öğrenci ve personele özel requestler yapılandırdık . Öğrenci ve personel apiyi tamamladık.',
+'tasarım düzenlendi, hesap düzeltme sayfası olusturuldu.',
+];
